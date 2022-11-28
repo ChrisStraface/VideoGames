167ed835bbe3490d327d7377692908d3c91eb8ee
ChrisStraface <chstraface@gmail.com>
2022-11-28 14:53:30 -0500

    Added first object to array list (Minecraft)

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ import java.util.ArrayList;
 public class Main {
     private static void main(String[] args) {
         ArrayList<VideoGames> allVideoGames = new ArrayList<>();
+    VideoGames Minecraft = new VideoGames("Minecraft", "multiplatform", "November 18, 2011");
+    allVideoGames.add(Minecraft);
     }
 }
