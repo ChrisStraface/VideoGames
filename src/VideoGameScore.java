08edf478dd94a69b37431b5fbe2f89b5c5d85520
ChrisStraface <chstraface@gmail.com>
2022-11-28 14:18:33 -0500

    Added SuperClass and WIP subclass

---
+++ src/VideoGameScore.java
@@ -0,0 +1,13 @@
+public class VideoGameScore extends VideoGames{
+
+    int rank;
+    int score;
+
+    private VideoGameScore(String name, String platform, String releaseDate, int rank, int score);
+    super(name, platform, releaseDate, int rank, int score);
+    return this.rank;
+    return this.score;
+
+    void describeSelf(name + ", " + platform + ", " + releaseDate + "ranked " + rank + ", score of " + score);{
+    }
+}
--- src/VideoGames.java
+++ src/VideoGames.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class VideoGames {
+public class VideoGames {
+     String name;
+     String platform;
+     String releaseDate;
+
+    private class VideoGames(String name,String Platform, String releaseDate);
+
+    void describeSelf(){
+        System.out.println("I am a video game I have a " + name + ", a " + platform + "and a " + releaseDate);
+    }
+
 }
