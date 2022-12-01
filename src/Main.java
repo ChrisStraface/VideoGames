eaa984175587293fcf1fa33d793873f441454ae8
ChrisStraface <chstraface@gmail.com>
2022-12-01 10:29:29 -0500

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
 
 public class Main {
-    private static void main(String[] args) {
+    public static void main(String[] args) {
         new VideoGameScore("The Legend of Zelda: Ocarina of Time", "N64", "November 23, 1998", 1, 99);
-new VideogamesSells("Minecraft","Multiplatform","November 18, 2011", "Mojand Studios", "Xbox Game studios", "Minecraft", 238000000);
+        new VideogamesSells("Minecraft","Multiplatform","November 18, 2011", "Mojand Studios", "Xbox Game studios", "Minecraft", 238000000);
+       for(VideoGames game: VideoGames.getAllVideoGames()) {
+           game.describeSelf();
+       }
     }
-
 
 }
 
--- src/VideoGameScore.java
+++ src/VideoGameScore.java
@@ -3,7 +3,7 @@ public class VideoGameScore extends VideoGames {
     private int rank;
     private int score;
 
-    private VideoGameScore(String name, String platform, String releaseDate, int rank, int score) {
+    VideoGameScore(String name, String platform, String releaseDate, int rank, int score) {
         super(name, platform, releaseDate);
         this.rank = rank;
         this.score = score;
