aed47170bcfc2516a1365a614a52bc28d118a70f
samjenkins <shinobislayer12@gmail.com>
2022-12-01 10:22:29 -0500

    Ye

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 public class Main {
     private static void main(String[] args) {
         new VideoGameScore("The Legend of Zelda: Ocarina of Time", "N64", "November 23, 1998", 1, 99);
-
+new VideogamesSells("Minecraft","Multiplatform","November 18, 2011", "Mojand Studios", "Xbox Game studios", "Minecraft", 238000000);
     }
+
+
 }
+
