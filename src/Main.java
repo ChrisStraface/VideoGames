ed9c620ecfb321882f49d83f7635c282a96382e2
samjenkins <shinobislayer12@gmail.com>
2022-12-15 10:35:58 -0500

    Ye

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         VideogamesSells.readAllData();
+
         VideoGameScore.readAllData();
         for(VideoGames game: VideoGames.getAllVideoGames()) {
             System.out.println(game);
